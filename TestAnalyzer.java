f30d7031ad9b5d17ded45df8ec0f83b2bc44f69b
ooption <p.karnaukhov@gmail.com>
2016-03-03 21:52:19 +0200

    Added files via upload

--- TestAnalyzer.java
+++ TestAnalyzer.java
@@ -1,16 +1,14 @@
 public class TestAnalyzer {
 
-	public static void main(String[] args) {
-		
-		int[] arr = {0, 1, 7, 8, 9, 15, 17, 20, 55, 56};
-		
-		
-		Analyzer testA = new Analyzer();
-		System.out.println(testA.searchClusters(arr));
-		
-		for(Cluster aa : testA.clusters) {	
-			System.out.println("Minimum found " + aa.minimum + ", cluster from " + aa.clusterstart + " to " + aa.clusterend);
-		}
+	public static int[] arr = {0, 1, 7, 8, 9, 15, 17, 20, 55, 56};
 
+	public static void main(String[] args) {				
+		Analyzer test = new Analyzer();
+		System.out.println(test.searchClusters(arr));
+		
+		for(Cluster aa : test.clusters) {	
+			System.out.println("Minimum found " + aa.minimum + ", cluster from " 
+								+ aa.clusterstart + " to " + aa.clusterend);
+		}
 	}
 }
